e0cdb765fae729f41feceee9f2f235af534b4c90
Flavio4ug <133663064+Flavio4ug@users.noreply.github.com>
2024-05-15 16:37:27 -0300

    Add files via upload

---
+++ entrada/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+namespace Entrada
+{
+    class Projeto
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int hora;
+            float salario;
+
+            Console.WriteLine("qual seu N°?");
+            numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("qual seu salario?");
+            salario = float.Parse(Console.ReadLine());
+            Console.WriteLine("quantas horas voce trabalha?");
+            hora = int.Parse(Console.ReadLine());
+
+            double salariomensal = hora * salario;
+
+            Console.WriteLine("OLÁ NÚMERO " + numero + ", o seu salário é: R$" + salariomensal);
+        }
+    }
+}
